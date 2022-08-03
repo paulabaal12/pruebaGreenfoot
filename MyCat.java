9affb0e6f58882c14636dead887db725a2cedfa3
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2022-08-03 06:49:41 -0600

    PROYECTO FATCAT

---
+++ MyCat.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * MyCat is your own cat. Get it to do things by writing code in its act method.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyCat extends Cat
+{
+    /**
+     * Act - do whatever the MyCat wants to do.
+     */
+    public void act()
+    {
+        eat ();
+        dance ();
+        sleep(2);
+        eat ();
+        dance ();
+        shoutHooray ();
+        move (6);
+        turn (5);
+        sleep(1);
+        eat ();
+        walkRight (5);
+        walkLeft (5);
+    }    
+}
